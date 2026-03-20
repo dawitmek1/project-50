58a8bc89221aec7e7ce692345fa4b08cab72b65e
dawitmek1 <dawitmek11@gmail.com>
2026-03-19 23:11:12 -0400

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-const CACHE = 'discipline-v1';
-const ASSETS = [
-  '/project-50/',
-  '/project-50/index.html',
-  '/project-50/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
-      caches.match('/project-50/index.html')
-    ))
-  );
-});
+const CACHE='disc7-v3';
+const ASSETS=['/project-50/','/project-50/index.html','/project-50/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/project-50/index.html'))));});
