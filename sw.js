01acbac0c880a9dc3ad2a857d1d7b87cbb13546a
dawitmek1 <dawitmek11@gmail.com>
2026-03-19 23:36:25 -0400

    Update cache version from v3 to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='disc7-v3';
+const CACHE='disc7-v4';
 const ASSETS=['/project-50/','/project-50/index.html','/project-50/manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
